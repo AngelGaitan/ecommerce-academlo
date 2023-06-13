6c5d66e07c15577279ac6be9d9cbfec20a8c08a6
Angel Gaitan <133923765+AngelGaitan@users.noreply.github.com>
2023-06-12 22:28:06 -0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+import * as darkMode  from './components/darkMode.js';
+
+import * as loader from './components/loader.js'
+
+import * as navM from './components/navMenu.js'
+
+import * as showCart from './components/showCart.js'
+import products from './components/products.js';
+import getProducts from './helpers/getProducts.js';
+import cart from './components/cart.js'; 
+
+const { db, printProducts} = products(await getProducts())
+cart(db, printProducts)
+
+
+
